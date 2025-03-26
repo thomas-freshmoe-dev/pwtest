f30e880113f73a85a4bbf6d2d3f92b354dbc1d74
thomas132-obs-dev <thantlynnnaing@obs.com.mm>
2025-03-26 13:44:47 +0630

    icon route fix

--- app/icons/icon-192x192.png/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import logo192 from "./icon-192x192.png";
-
-const page = () => {
-  return <Image src={logo192} alt="logo192" />;
-};
-
-export default page;
--- app/icons/icon-512x512.png/page.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import icon512 from "./icon-512x512.png";
-
-const page = () => {
-  return <Image src={icon512} alt="logo512" />;
-};
-
-export default page;
