5a3696dadd175fa016ac69ce198f3d3b59dbb3fa
thomas132-obs-dev <thantlynnnaing@obs.com.mm>
2025-03-26 13:40:20 +0630

    icon test

---
+++ app/icons/icon-192x192.png/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import logo192 from "./icon-192x192.png";
+
+const page = () => {
+  return <Image src={logo192} alt="logo192" />;
+};
+
+export default page;
---
+++ app/icons/icon-512x512.png/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import icon512 from "./icon-512x512.png";
+
+const page = () => {
+  return <Image src={icon512} alt="logo512" />;
+};
+
+export default page;
